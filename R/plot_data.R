6a1634be2367112698d9fde096e07398eca89b5b
Przemyslaw Miskowiec <przemekmiskowiec@gmail.com>
2024-07-13 21:31:07 +0200

    Hotfix example plot

--- R/plot_data.R
+++ R/plot_data.R
@@ -9,42 +9,55 @@
 #' @return A ggplot object.
 #' @export
 #' @examples
-#' library(ggplot2)
-#' # Example using mtcars dataset for line plot
-#' data <- mtcars
-#' autoplot(data, "cyl", "mpg", "line")
-#' autoplot(data, "cyl", "mpg", "scatter")
-#' autoplot(data, "cyl", "NULL", "histogramm")
-#' autoplot(data, "cyl", "mpg", "bar")
- autoplot <- function(data, x , y = NULL, plot_type = "line") {
-   if (!requireNamespace("ggplot2", quietly = TRUE)) {
-     stop("ggplot2 package is required but not installed.")
-   }
+#' \dontrun{
+#'   library(ggplot2)
+#'   data <- mtcars
+#'
+#'   # Example using mtcars dataset for line plot
+#'   p <- autoplot(data, "cyl", "mpg", "line")
+#'   print(p)
+#'
+#'   # Example using mtcars dataset for scatter plot
+#'   p <- autoplot(data, "cyl", "mpg", "scatter")
+#'   print(p)
+#'
+#'   # Example using mtcars dataset for histogram
+#'   p <- autoplot(data, "mpg", plot_type = "histogram")
+#'   print(p)
+#'
+#'   # Example using mtcars dataset for bar plot
+#'   p <- autoplot(data, "cyl", plot_type = "bar")
+#'   print(p)
+#' }
+autoplot <- function(data, x, y = NULL, plot_type = "line") {
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    stop("ggplot2 package is required but not installed.")
+  }
 
-   if (!x %in% names(data)) {
-     stop(paste("Column", x, "not found in data."))
-   }
+  if (!x %in% names(data)) {
+    stop(paste("Column", x, "not found in data."))
+  }
 
-   if (!is.null(y) && !y %in% names(data)) {
-     stop(paste("Column", y, "not found in data."))
-   }
+  if (!is.null(y) && !y %in% names(data)) {
+    stop(paste("Column", y, "not found in data."))
+  }
 
-   p <- ggplot2::ggplot(data, ggplot2::aes_string(x = x, y = y))
+  p <- ggplot2::ggplot(data, ggplot2::aes_string(x = x, y = y))
 
-   if (plot_type == "line") {
-     p <- p + ggplot2::geom_line()
-   } else if (plot_type == "scatter") {
-     p <- p + ggplot2::geom_point()
-   } else if (plot_type == "histogramm") {
-     if (!is.null(y)) {
-       warning("Ignoring y parameter for histogram plot type.")
-     }
-     p <- ggplot2::ggplot(data, ggplot2::aes_string(x = x)) + ggplot2::geom_histogram()
-   } else if (plot_type == "bar") {
-     p <- p + ggplot2::geom_bar(stat = "identity")
-   } else {
-     stop("Unsupported plot type")
-   }
+  if (plot_type == "line") {
+    p <- p + ggplot2::geom_line()
+  } else if (plot_type == "scatter") {
+    p <- p + ggplot2::geom_point()
+  } else if (plot_type == "histogram") {
+    if (!is.null(y)) {
+      warning("Ignoring y parameter for histogram plot type.")
+    }
+    p <- ggplot2::ggplot(data, ggplot2::aes_string(x = x)) + ggplot2::geom_histogram()
+  } else if (plot_type == "bar") {
+    p <- p + ggplot2::geom_bar(stat = "identity")
+  } else {
+    stop("Unsupported plot type")
+  }
 
-   return(p)
- }
+  return(p)
+}
